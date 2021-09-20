9afaa64b4dc8b74689f329e3261b63ee1002e2e7
theInternauts <cricketw@wearetheinternauts.com>
2021-09-20 13:04:49 +0900

    configure cocoapods

---
+++ Pods/Target Support Files/Pods-FavoriteRecipes-FavoriteRecipesUITests/Pods-FavoriteRecipes-FavoriteRecipesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FavoriteRecipes_FavoriteRecipesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FavoriteRecipes_FavoriteRecipesUITests
+@end
